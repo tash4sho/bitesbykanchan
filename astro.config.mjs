ebdbbc1f8e214822bb60f6a71e850a2fe0e236cf
tash4sho <successdiscipline@gmail.com>
2026-04-29 09:24:16 -0400

    update sirte url custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   // Replace with your real domain when you deploy. Used for canonical URLs and sitemap.
-  site: 'https://bitesbykanchan.successdiscipline.workers.dev/',
+  site: 'https://bitesbykanchan.com/',
   compressHTML: true,
   build: {
     inlineStylesheets: 'auto',
